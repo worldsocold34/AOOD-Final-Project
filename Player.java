7301d67fabc032017e41e6f538fb86eea9f8f979
worldsocold34 <worldsocold34@users.noreply.github.com>
2017-03-22 14:00:46 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,57 @@
+
+public class Player {
+
+	/**
+	 * @param args
+	 */
+	
+	private String[] elements = {"fire", "ice", "earth", "wind"};
+	private int[] defense = {100, 100, 100, 100};//player defense percentages
+	private int[][] weaponsRange = {{5, 5, 5, 5},{10, 10, 10, 10}};//lower range of weapon, higher range of weapon
+	private int maxHP;
+	private int HP;
+	private int count;
+	
+	public Player() {
+		count = 0;
+		HP = 100;
+		maxHP = 100;
+	}
+	
+	public int attack(int weapon, int[] monsterDefense) {
+		int min = weaponsRange[0][weapon];
+		int max = weaponsRange[1][weapon];
+		int damage = (int)((max-min+1)*Math.random()+min);
+		return damage*monsterDefense[weapon]/100;
+	}
+	
+	public void heal() {
+		HP += (int)(6*Math.random()+5);
+	}
+	
+	public void increaseWeaponsRange(int weapon) {
+		weaponsRange[0][weapon]++;
+		weaponsRange[1][weapon]++;
+	}
+	
+	public void increaseMaxHP(int points) {
+		maxHP =+ points;
+	}
+	
+	public void increaseCount() {
+		count++;
+	}
+	
+	public int getHP() {
+		return HP;
+	}
+	
+	public void getDefense() {
+		// display player stats
+	}
+	
+	public int getPlayerCount() {
+		return count;
+	}
+
+}
