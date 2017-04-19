a39a517c23e0f4aa6502af29f444692d2f1c82a3
worldsocold34 <worldsocold34@users.noreply.github.com>
2017-04-19 13:26:46 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,17 @@
+
+public class Game {
+
+	public static int count;
+	public static String[] elements = {"fire", "ice", "earth", "wind"};
+	private Player player;
+	public Game() {
+		count = 0;
+		player = new Player();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
