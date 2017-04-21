727f3809cac857e6547a888c5fbe9b8535157593
worldsocold34 <worldsocold34@users.noreply.github.com>
2017-04-21 14:09:40 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,11 +7,29 @@ public class Game {
 	public Game() {
 		count = 0;
 		player = new Player();
+		play(player);
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void turn(Monster monster, int weapon) {//when player attacks
+		while((player.getHP()>0)&&(monster.getHP()>0)) {
+			int playerDmg = player.attack(weapon, monster.getDefense());
+			monster.takeDamage(playerDmg);
+			int monsterDmg = monster.attack(player.getDefense());
+			player.takeDamage(monsterDmg);
+		}
+		
+	}
+	
+	public void turn(Monster monster) {
+		while((player.getHP()>0)&&(monster.getHP()>0)) {
+			
+		}
+		
+	}
+	
+	public void play(Player player) {
+		Monster monster = new Monster(player);
+		
 	}
 
 }
